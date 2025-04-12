e0c0dcfe721de824afa5ebd0740a488302c2467f
Prakash0Singh <prakash0basnal0@gmail.com>
2025-04-12 10:41:44 +0530

    basic setup of components

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Login.css'
+
+function Login() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Login
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Register.css'
+
+function Register() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Register
